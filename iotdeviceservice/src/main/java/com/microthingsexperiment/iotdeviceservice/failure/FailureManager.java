9d7f061416dc1da2243d362318202a3d25d4407e
Gibeon Aquino <gibeon@dimap.ufrn.br>
2019-03-31 20:48:22 +0100

    Ajuste no comportamenta das falhas e setup do log

---
+++ iotdeviceservice/src/main/java/com/microthingsexperiment/iotdeviceservice/failure/FailureManager.java
@@ -0,0 +1,68 @@
+package com.microthingsexperiment.iotdeviceservice.failure;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.microthingsexperiment.iotdeviceservice.Setup;
+
+@Component
+public class FailureManager {
+
+	@Value("${failure.file.path}")
+	private String failureFilePath;
+
+	@Autowired
+	private Setup setup;
+
+	private List<FailureOcurrence> failures = new ArrayList<FailureOcurrence>();
+
+	@PostConstruct
+	private void loadFailureFile() {
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(failureFilePath));
+			for (String line : lines) {
+				String[] lineValues = line.split(";");
+				for (String value : lineValues) {
+					String[] split = value.split(":");
+					Integer failureMoment = Integer.valueOf(split[0]);
+					Integer failureDuration = Integer.valueOf(split[1]);
+					failures.add(new FailureOcurrence(failureMoment*1000l, failureDuration*1000l));
+				}
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean isFailed() {
+		long executionTime = setup.getExecutionTime();
+		boolean isFailed = false;
+
+		if (setup.isActive()) {
+
+			for (FailureOcurrence failure : failures) {
+
+				if (executionTime >= failure.getStart() && executionTime <= failure.getEnd()) {
+					isFailed = true;
+					break;
+				}
+			}
+
+		}
+		
+		return isFailed;
+
+	}
+
+}
