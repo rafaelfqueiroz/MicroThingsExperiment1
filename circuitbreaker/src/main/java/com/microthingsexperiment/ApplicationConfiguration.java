7c23c879a019037eef6a438d832e2c9b652cfe05
rafaelfq <rfqueiroz.91@gmail.com>
2019-04-01 15:45:14 -0300

    Extraiondo bean do rest template para outra classe de configuração

---
+++ circuitbreaker/src/main/java/com/microthingsexperiment/ApplicationConfiguration.java
@@ -0,0 +1,24 @@
+package com.microthingsexperiment;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationConfiguration {
+
+	@Value("${timeout}")
+	private long timeout;
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplateBuilder builder = new RestTemplateBuilder();
+		return builder.setConnectTimeout(Duration.ofMillis(timeout))
+				.setReadTimeout(Duration.ofMillis(timeout)).build();
+	}
+	
+}
--- circuitbreaker/src/main/java/com/microthingsexperiment/CircuitBreakerConfig.java
+++ circuitbreaker/src/main/java/com/microthingsexperiment/CircuitBreakerConfig.java
@@ -1,27 +1,12 @@
 package com.microthingsexperiment;
 
-import java.time.Duration;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableCircuitBreaker
 @Profile("EnableCircuitBreaker")
 public class CircuitBreakerConfig {
 
-	@Value("${timeout}")
-	private long timeout;
-	
-	@Bean
-	public RestTemplate restTemplate() {
-		RestTemplateBuilder builder = new RestTemplateBuilder();
-		return builder.setConnectTimeout(Duration.ofMillis(timeout))
-				.setReadTimeout(Duration.ofMillis(timeout)).build();
-	}
 }
