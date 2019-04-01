60d2402cbdf269fefa9fcdbe10ae926e6582e169
rafaelfq <rfqueiroz.91@gmail.com>
2019-04-01 14:28:00 -0300

    CircuitBreaker habilitado através de definição do profile EnableCircuitBreaker.

--- caller/src/main/java/com/microthingsexperiment/caller/CallerApplication.java
+++ caller/src/main/java/com/microthingsexperiment/caller/CallerApplication.java
@@ -2,10 +2,8 @@ package com.microthingsexperiment.caller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication(scanBasePackages="com.microthingsexperiment")
-@EnableCircuitBreaker
 public class CallerApplication {
 
 	public static void main(String[] args) {
--- iotgateway/src/main/java/com/microthingsexperiment/iotgateway/IoTGatewayApplication.java
+++ iotgateway/src/main/java/com/microthingsexperiment/iotgateway/IoTGatewayApplication.java
@@ -2,10 +2,8 @@ package com.microthingsexperiment.iotgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication(scanBasePackages="com.microthingsexperiment")
-@EnableCircuitBreaker
 public class IoTGatewayApplication {
 
 	public static void main(String[] args) {
